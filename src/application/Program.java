0c7d490355e7751be6a089d454858d216038076d
Lorenzo Amorim <lorenzoamorim@hotmail.com>
2023-05-30 22:24:39 -0300

    Criando uma classe com três atributos para representar melhor o triângulo

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Triangle;
+
 public class Program {
 
 	//RESOLVENDO UM PROBLEMA SEM A ORIENTAÇÃO A OBJETOS
@@ -11,22 +13,27 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		double xA, xB, xC, yA, yB, yC;
+		//definindo as variáveis das medidas dos lados dos triângulos x e y
+		Triangle x, y;
+		//instanciar/criar o objeto
+		x = new Triangle();
+		y = new Triangle();
 		
 		System.out.println("Enter the measures of triangle X: ");
-		xA = sc.nextDouble();
-		xB = sc.nextDouble();
-		xC = sc.nextDouble();
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
 		System.out.println("Enter the measures of triangle Y: ");
-		yA = sc.nextDouble();
-		yB = sc.nextDouble();
-		yC = sc.nextDouble();
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
 		
-		double p = (xA + xB + xC)/ 2;
-		double areaX = Math.sqrt(p*(p-xA)*(p-xB)*(p-xC));
 		
-		p = (yA + yB + yC)/2;
-		double areaY = Math.sqrt(p*(p-yA)*(p-yB)*(p-yC));
+		double p = (x.a + x.b + x.c)/ 2;
+		double areaX = Math.sqrt(p*(p-x.a)*(p-x.b)*(p-x.c));
+		
+		p = (y.a + y.b + y.c)/2;
+		double areaY = Math.sqrt(p*(p-y.a)*(p-y.b)*(p-y.c));
 		
 		System.out.printf("Triangle X area: %.4f%n", areaX);
 		System.out.printf("triangle Y area: %.4f%n", areaY);
---
+++ src/entities/Triangle.java
@@ -0,0 +1,9 @@
+package entities;
+
+public class Triangle {
+	
+	//declarar os atributos das classes
+	public double a;
+	public double b;
+	public double c;
+}
