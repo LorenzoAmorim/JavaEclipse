1dc3552501e8b0e1823259d5adfe715cca8c3b9f
Lorenzo Amorim <lorenzoamorim@hotmail.com>
2023-05-30 22:27:10 -0300

    Criando uma classe com três atributos para representar melhor o triângulo

--- src/entities/Triangle.java
+++ src/entities/Triangle.java
@@ -2,7 +2,7 @@ package entities;
 
 public class Triangle {
 	
-	//declarar os atributos das classes
+	//declarar os atributos das classes para adicionar ao programa principal
 	public double a;
 	public double b;
 	public double c;
